d1f8446329bcf80dae3c3e63ae26d4c9521d965b
foo <zcrzcr@qq.com>
2021-07-06 21:40:35 +0800

    torn015

--- Scripts/torn.js
+++ Scripts/torn.js
@@ -195,7 +195,7 @@ class Widget extends Base {
   constructor(arg) {
     super(arg)
     this.name = 'TORN 小组件'
-    this.version = '0.1.4'
+    this.version = '0.1.5'
     this.desc = `版本 ${this.version}`
   }
 
