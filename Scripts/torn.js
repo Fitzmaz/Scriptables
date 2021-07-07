9981d8f78350e7f793b7743b509b527bbbc9a4aa
foo <zcrzcr@qq.com>
2021-07-08 00:41:02 +0800

    [torn] 自有PI剩余天数显示为∞

--- Scripts/torn.js
+++ Scripts/torn.js
@@ -45,6 +45,7 @@ const cooldownsChar = {
   [DataKeyMedical]: '医',
   [DataKeyRacing]: '车'
 }
+const DaysLeftForever = 999
 const RacingStatusWaiting = 'RacingStatusWaiting'
 const RacingStatusRacing = 'RacingStatusRacing'
 const RacingStatusFinished = 'RacingStatusFinished'
@@ -221,6 +222,9 @@ class Widget extends Base {
         const data = await this.httpGet(propertyAPI, true, false)
         if (data && data.property && data.property.rented) {
           days = data.property.rented.days_left
+          if (typeof days === 'string') {
+            days = DaysLeftForever
+          }
           storage.setJSON(PropertyDataKey, { days, timestamp: Date.now()})
         } else {
           console.warn('invalid property data')
@@ -230,7 +234,7 @@ class Widget extends Base {
     } else {
       result = {"timestamp":1615639666,"level":25,"gender":"Male","player_id":2587304,"name":"microdust","server_time":1615639666,"points":36,"cayman_bank":0,"vault_amount":0,"daily_networth":5379662754,"money_onhand":600293,"education_current":61,"education_timeleft":1545972,"status":{"description":"Traveling to United Kingdom","details":"","state":"Traveling","color":"blue","until":0},"travel":{"destination":"United Kingdom","timestamp":1615644140,"departed":1615637300,"time_left":4474},"cooldowns":{"drug":27118,"medical":17393,"booster":13236},"happy":{"current":4938,"maximum":5025,"increment":5,"interval":900,"ticktime":734,"fulltime":16034},"life":{"current":685,"maximum":1181,"increment":70,"interval":300,"ticktime":134,"fulltime":2234},"energy":{"current":30,"maximum":150,"increment":5,"interval":600,"ticktime":134,"fulltime":13934},"nerve":{"current":15,"maximum":61,"increment":1,"interval":300,"ticktime":134,"fulltime":13634},"chain":{"current":0,"maximum":10000,"timeout":0,"modifier":1,"cooldown":0},"city_bank":{"amount":2436000000,"time_left":6732555},"education_completed":[14,18,19,20,34,43,44,45,46,47,48,49,50,51,52,54,112,113,126,127],"refills":{"energy_refill_used":true,"nerve_refill_used":false,"token_refill_used":false,"special_refills_available":0},"icons":{"icon6":"Male","icon4":"Subscriber - Donator status: 92 days - Subscriber until: 24/08/21","icon8":"Married - To Trefor","icon29":"Bank Investment - Current bank investment worth $3,639,000,000 - 60 days, 23 hours, 34 minutes and 39 seconds","icon27":"Company - Chandler of Lead Farmers (Candle Shop)","icon9":"Faction - Karajan of November Chopin","icon19":"Education - Currently completing the Bachelor of Psychological Sciences course - 26 days, 9 hours, 9 minutes and 58 seconds","icon38":"Stock Market - You own shares in the stock market","icon85":"Organized Crime - Planned Robbery - 3 days, 15 hours, 47 minutes and 0 seconds","icon39":"Booster Cooldown - 01:24:28 / 24:00:00","icon52":"Drug Cooldown - Under the influence of Xanax - 03:03:50 ","icon78":"Property Upkeep war - $21,755,000 is due in property upkeep","icon17":"Racing - Waiting for a race to start - 00:58:29"}}
       result.timestamp = Math.floor(Date.now() / 1000)
-      piDaysLeft = 88
+      piDaysLeft = DaysLeftForever
     }
     const { player_id } = result
     if (player_id) {
@@ -290,7 +294,16 @@ class Widget extends Base {
       }
       const tokenBGColor = Color.dynamic(new Color('#ececec', 0.5), new Color('#333333', 0.5))
       let timeLeftObject = formatTimeLeft(data[key])
-      let timeLeftString = timeLeftObject ? `${timeLeftObject.value}${timeLeftObject.unit[0]}` : `now`
+      let timeLeftString
+      if (timeLeftObject) {
+        if (timeLeftObject.value === DaysLeftForever) {
+          timeLeftString = ' ∞'
+        } else {
+          timeLeftString = `${timeLeftObject.value}${timeLeftObject.unit[0]}`
+        }
+      } else {
+        timeLeftString = `now`
+      }
       rightTokenOptions.push(new TokenOption(`${names[key]}${timeLeftString}`, null, tokenBGColor))
     }
     //
